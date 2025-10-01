aaced082ef84a8efa74964d45937aa3e7533ccd7
Victoria Raznodumova <raznodumova@gmail.com>
2025-10-01 21:17:11 +0300

    realizing i/o from lesson-19

---
+++ iprody-temporary/src/io/Book.java
@@ -0,0 +1,49 @@
+package io;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private String title;
+    private String author;
+    private Person person;
+
+
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", person=" + person +
+                '}';
+    }
+}
